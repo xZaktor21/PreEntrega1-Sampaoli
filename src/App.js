77229b66e788bf59d80b5d5944bb0b41b34daa2e
Zaktor21 <sampaolifelipe@gmail.com>
2023-06-07 19:52:26 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-import React from 'react';
-import '@fortawesome/fontawesome-free/css/all.css';
-import NavBar from './components/NavBar/NavBar';
-import ItemListContainer from './components/ItemListContainer/ItemListContainer';
-
-
 
 function App() {
   return (
     <div className="App">
-      
-      <NavBar/>
-
-      <ItemListContainer greeting = {"Hola gente"}/>
-
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
--- src/components/ItemListContainer/ItemListContainer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const ItemListContainer = (props) => {
-    return (
-        <h3> {props.greeting} </h3>
-    )
-}
-
-export default ItemListContainer
